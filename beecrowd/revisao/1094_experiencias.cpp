ee433a91009afc607d65a7de0ba4024ab58baecf
Caroline Ferreira de Souza <carolferreira.cf699@gmail.com>
2026-08-30 18:29:04 -0300

    novo - adicionar 1094_experiencias do beecrowd - revisão

--- beecrowd/revisao/1094_experiencias.cpp
+++ beecrowd/revisao/1094_experiencias.cpp
@@ -23,6 +23,7 @@ PERCENTUAL DEVE SER APRESENTADO COM DOÍS DÍGITOS APÓS O PONTO.
 
 //biblioteca
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -30,12 +31,66 @@ using namespace std;
 int main()
 {
     //declaração de dados
+    int n; //varios casos de teste
+    int quantia;
+    char tipo; //tipo cobaia
+    
+    //inicializar com zero
+    int total_cobaias = 0, total_coelhos = 0, total_ratos = 0, total_sapos = 0;
+
+    //calculos do percentuais
+    float percentual_coelhos = 0.0, percentual_ratos = 0.0, percentual_sapos = 0.0;
 
     //entrada de dados
+    //ler o casos de teste
+    cin >> n;
+
+    //ler os casos
+    for (int i = 0; i < n; i++)
+    {
+        //ler a quantia e o tipo
+        cin >> quantia >> tipo;
+
+        //desenvolvimento
+        //acumula no total
+        total_cobaias += quantia;
+
+        //verifica o tipo da cobaia e acumular
+        if (tipo == 'C')
+        {
+            total_coelhos += quantia;
+        }
+        else if (tipo == 'R')
+        {
+            total_ratos += quantia;
+        }
+        else if (tipo == 'S')
+        {
+            total_sapos += quantia;
+        }
+
+    }
 
     //desenvolvimento
+    //verificar a divisão
+    if (total_cobaias > 0)
+    {
+        //float - para uma divisão inteira, que resulta em zero.
+        percentual_coelhos = (float)total_coelhos / total_cobaias * 100.0;
+        percentual_ratos = (float)total_ratos / total_cobaias * 100.0;
+        percentual_sapos = (float)total_sapos / total_cobaias * 100.0;
+    }
 
     //saída de dados
+    cout << "Total: " << total_cobaias << " cobaias" << endl;
+    cout << "Total de coelhos: " << total_coelhos << endl;
+    cout << "Total de ratos: " << total_ratos << endl;
+    cout << "Total de sapos: " << total_sapos << endl;
+
+    cout << fixed << setprecision(2);
+    cout << "Percentual de coelhos: " << percentual_coelhos << " %" << endl;
+    cout << "Percentual de ratos: " << percentual_ratos << " %" << endl;
+    cout << "Percentual de sapos: " << percentual_sapos << " %" << endl;
 
     return 0;
 }
